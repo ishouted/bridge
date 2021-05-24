4eaefc6644cfbf9f024fcfa09208d75f5dc24003
huangsheng <huangsheng@nuls.io>
2021-05-24 16:55:08 +0800

    暂时修改beta配置为主网配置

--- config/beta.js
+++ config/beta.js
@@ -1,5 +1,5 @@
 
-//开发模式
+/* //开发模式
 export const IS_DEV = process.env.NODE_ENV === 'development';
 
 //燃烧地址的公钥
@@ -12,5 +12,20 @@ export const NULS_INFO = { chainId: 2, assetId: 1, prefix: 'tNULS',symbol:"NULS"
 //ETH 网络信息 测试网:ropsten, 主网:homestead
 export const ETHNET = 'ropsten';
 
-export const BRIDGE_API_URL = "/bridge-api";
+export const BRIDGE_API_URL = "/bridge-api"; */
+
+//开发模式
+export const IS_DEV = process.env.NODE_ENV === 'development';
+
+//燃烧地址的公钥
+export const API_BURNING_ADDRESS_PUB = 'NERVEepb63T1M8JgQ26jwZpZXYL8ZMLdUAK31L';
+
+//ChainId和资产ID
+export const MAIN_INFO = { chainId: 9, assetId: 1, prefix: 'NERVE', symbol: "NVT", decimal: 8, rpc: "https://public.nerve.network" };
+export const NULS_INFO = { chainId: 1, assetId: 1, prefix: 'NULS',symbol:"NULS", decimal: 8, rpc: "https://public1.nuls.io" };
+
+//ETH 网络信息 测试网:ropsten, 主网:homestead
+export const ETHNET = 'homestead';
+
+export const BRIDGE_API_URL = "/beta-bridge-api";
 
