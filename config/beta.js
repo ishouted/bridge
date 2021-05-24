289d697e42b7ba2239482c55e6787a216e766797
huangsheng <huangsheng@nuls.io>
2021-05-24 17:12:04 +0800

    api去掉beta

--- config/beta.js
+++ config/beta.js
@@ -27,5 +27,5 @@ export const NULS_INFO = { chainId: 1, assetId: 1, prefix: 'NULS',symbol:"NULS",
 //ETH 网络信息 测试网:ropsten, 主网:homestead
 export const ETHNET = 'homestead';
 
-export const BRIDGE_API_URL = "/beta-bridge-api";
+export const BRIDGE_API_URL = "/bridge-api";
 
